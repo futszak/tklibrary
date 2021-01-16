5db836aed9f5d069cd64e92a50116f2893c05ee8
futszaK <futszak@gmail.com>
2021-01-16 16:28:35 +0100

    added first function

--- tklibrary.py
+++ tklibrary.py
@@ -1,1 +1,25 @@
+#!/usr/local/bin/python3.9
+# -*- coding:utf-8 -*-
+# Name: TK Library
+# Discription: Communication Python Library by Tomasz Kruk
 #
+# Author:  Tomasz Kruk   futszak@gmail.com
+# version 0.1
+
+import socket
+import configparser
+
+configini = configparser.ConfigParser()
+configini.read('config.ini')
+
+try:
+    UDP_PORT=configini['logsend']['port']
+except:
+    UDP_PORT=514
+
+
+def logsend(x):
+    if not configini['logsend']['address']:
+        return()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(x.encode(), (configini['logsend']['address'], UDP_PORT))
